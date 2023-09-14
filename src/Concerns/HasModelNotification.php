fdc56ee0ec3e353d0c0666eea3a5db607c2dac1d
Adam Lee <adam@codelabx.ltd>
2023-09-14 23:27:20 +0100

    HasModelNotification refactor generic

--- src/Concerns/HasModelNotification.php
+++ src/Concerns/HasModelNotification.php
@@ -5,46 +5,28 @@ namespace XtendLunar\Features\NotifyTimeline\Concerns;
 use Filament\Notifications\Actions\Action;
 use Filament\Notifications\DatabaseNotification;
 use Illuminate\Support\Str;
-use Lunar\Models\Customer;
-use Lunar\Models\Order;
 use XtendLunar\Features\NotifyTimeline\Base\Notification;
 
 trait HasModelNotification
 {
-    protected static function customerNotification(Customer $customer): DatabaseNotification
+    protected static function makeNotification(
+        string $type,
+        string $title,
+        string $body,
+        ?string $route,
+    ): DatabaseNotification
     {
         return Notification::make()
-            ->success()
+            ->status($type)
             ->system()
             ->id(Str::random())
-            ->title('New customer registered successfully!')
-            ->body('System detected new customer registration named **'.$customer->fullName.'**.')
-            ->actions([
+            ->title($title)
+            ->body($body)
+            ->actions($route ? [
                 Action::make('view')
                     ->button()
-                    ->url(route('hub.customers.show', ['customer' => $customer])),
-            ])
+                    ->url($route),
+            ] : [])
             ->toDatabase();
-    }
-
-    protected static function orderNotification(Order $order): DatabaseNotification
-    {
-        return Notification::make()
-            ->success()
-            ->system()
-            ->id(Str::random())
-            ->title('New order placed successfully!')
-            ->body("**{$order->customer->fullName}** ordered **{$order->lines->count()}** products.")
-            ->actions([
-                Action::make('view')
-                    ->button()
-                    ->url(route('hub.orders.show', ['order' => $order])),
-            ])
-            ->toDatabase();
-    }
-
-    protected static function cartNotification()
-    {
-        // @todo Handle notification for abandon cart
     }
 }
